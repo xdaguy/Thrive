caf517210303244ff784a9748e0d1eb7fa95b60d
Abdullah Haris <abdullaharis111@gmail.com>
2025-11-16 18:39:10 +0000

    FIXED: Cross-Origin-Opener-Policy Issue

--- next.config.js
+++ next.config.js
@@ -39,6 +39,10 @@ const nextConfig = {
           {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups'
           }
         ]
       }
